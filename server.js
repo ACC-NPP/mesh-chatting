0721540c9ab4d8b8567ad75874bbe9602d8d1680
Venera Rodionova <c0ntent.dao.kz@gmail.com>
2023-05-12 18:48:18 +0300

    implement autodetection for platform, stage, network interface, ipv4, ipv6

--- server.js
+++ server.js
@@ -1,37 +1,70 @@
 const http = require('http');
+const {exec} = require('child_process');
 const fetch = require('node-fetch');
 const port = 5555;
-//const ipv4 = '127.0.0.1'; // local host
-const ipv4 = '192.168.1.136'; // local network
-//const ipv6 = '::1'; // local host
-const ipv6 = 'fe80::10b7:31d8:50a5:bf95%en0'; // local network
+const supported_platforms = {
+	'linux': {name: 'Debian/Ubuntu', stage: 'prod', network_interface: 'wlan1'}, 
+	'win32': {name: 'Windows 32/64', stage: 'test', network_interface: ''},
+	'darwin': {name: 'MacOS', stage: 'dev', network_interface: 'en0'},
+};
+const {LOCAL_RUN} = process.env;
+async function getMyIPv(version, interface) {
+	let processGetIp = () => {
+		return new Promise((resolve, reject) => {
+			function handleGetIp(error, stdout, stderr) {
+				if (error || stderr) {
+					stderr && console.log(`stderr: ${stderr}`);
+					error && console.log(`error: ${error.message}`);
+					reject(stderr);
+				} else {
+					const result = stdout.split('\n')[0];
+					console.log(`stdout: ${result}`);
+					resolve(result);
+				}
+			}
+			exec(`ifconfig ${interface} | grep "inet${version === 6 ? '6' : ''} " | awk '{print $2}'`, handleGetIp);		
+		});
+	};
+	return await processGetIp();
+}
 function getAddressUrl(a) {
 	const {family, address, port} = a.address();
-	if (family === 'IPv6')
-		return `\tlinux http://${address}:${port}/
-		windows http://${address.split('%')[0]}:${port}/
-		macos does not work :'(`;
-	else
-		return `http://${address}:${port}/`;
+	if (family === 'IPv6') {
+		if (process.platform === 'win32')
+			`http://[${address.split('%')[0]}]:${port}/`;
+		return `http://[${address}]:${port}/`;
+	}
+	return `http://${address}:${port}/`;
 }
 function getAddressDescription(a) {
 	const url = getAddressUrl(a);
 	const {family} = a.address();
 	return `${family} ${url}`;
 }
-const a = http
-	.createServer(async (req, res) => {
-		res.writeHead(200, { "Content-Type": "text/html" });
-		res.end('hello ipv6!');
-	}).listen(port, ipv6,
-		() => console.log(`run at ${getAddressDescription(a)}`));
-const b = http
-	.createServer(async (req, res) => {
-		//const response = await fetch(getAddressUrl(a));
-		const text = null;//await response.text();
+async function run() {
+	if (!(process.platform in supported_platforms)) {
+		process.exit(1);
+	}
+	const {name, stage, network_interface} = supported_platforms[process.platform];
+	console.log(`platform detected: ${process.platform} (${name}), stage ${stage}`);
 
+	const ipv4 = LOCAL_RUN ? '127.0.0.1' : await getMyIPv(4, network_interface);
+	const ipv6 = LOCAL_RUN ? '::1' : await getMyIPv(6, network_interface);
+	const a = http.createServer(async (req, res) => {
 		res.writeHead(200, { "Content-Type": "text/html" });
-		res.end(`hello ipv4! and bridge says: ${text}`);
-	})
-	.listen(port, ipv4,
-		() => console.log(`run at ${getAddressDescription(b)}`));
+		res.end(`hello IPv6! @ ${ipv6}`);
+	}).listen(port, ipv6, () => console.log(`run at ${getAddressDescription(a)}`));
+	const b = http.createServer(async (req, res) => {
+		let text = null;
+		try {
+			const response = await fetch(getAddressUrl(a));
+			text = await response.text();
+		}
+		catch(e) {
+			text = e.message;
+		}
+		res.writeHead(200, { "Content-Type": "text/html" });
+		res.end(`hello IPv4! and bridge says: ${text}. @ ${ipv6}`);
+	}).listen(port, ipv4, () => console.log(`run at ${getAddressDescription(b)}`));
+}
+run();
